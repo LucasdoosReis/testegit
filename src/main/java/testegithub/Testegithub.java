6d978f156ea75fb5fdadd7bfcd212edd9ebbe790
LucasdoosReis <lucaslladder@gmail.com>
2024-03-08 12:00:43 -0300

    commit inicio

---
+++ src/main/java/testegithub/Testegithub.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package testegithub;
+
+/**
+ *
+ * @author Lucas  Reis <lucaslladder@gmail.com>
+ */
+public class Testegithub {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
